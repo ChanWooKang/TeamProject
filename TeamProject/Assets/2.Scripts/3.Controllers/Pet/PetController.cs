e0606a606471d3c2bbfcdc712ced14458b6fd2e2
enjoingmediocrity <gnrotlf@naver.com>
2024-08-27 13:59:10 +0900

    YoonHo Commit
    Pet

---
+++ TeamProject/Assets/2.Scripts/3.Controllers/Pet/PetController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PetController : MonoBehaviour
+{
+    Vector3? m_targetPos = null;
+    private void Update()
+    {
+        if (m_targetPos != null)
+        {
+            if (Vector3.Distance(transform.position, m_targetPos.Value) < 2)
+            {
+                m_targetPos = null;
+                return;
+            }
+
+            transform.position = Vector3.MoveTowards(transform.position, m_targetPos.Value, 10f * Time.deltaTime);
+        }
+    }
+
+
+    public void MoveToObject(Vector3 targetPos)
+    {
+        m_targetPos = targetPos;
+    }
+
+    public void PatrolInOwnArea()
+    {
+
+    }
+
+
+}
