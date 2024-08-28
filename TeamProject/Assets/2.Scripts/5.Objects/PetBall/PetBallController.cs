c106db4f4aae441e9d8d6112fa0559a4da5d13fb
enjoingmediocrity <gnrotlf@naver.com>
2024-08-28 16:04:46 +0900

    YoonHo Commit
    버그 수정

--- TeamProject/Assets/2.Scripts/5.Objects/PetBall/PetBallController.cs
+++ TeamProject/Assets/2.Scripts/5.Objects/PetBall/PetBallController.cs
@@ -6,6 +6,7 @@ public class PetBallController : MonoBehaviour
 {    
     MonsterController m_targetMonsterCtrl;
     PetBallInfo m_petBallInfo;
+    GameObject m_player;
 
     float m_bonusCaptureRate;
 
@@ -18,6 +19,7 @@ public class PetBallController : MonoBehaviour
         if(other.CompareTag("Pet"))
         {
             //Æ÷È¹
+            m_player = GameObject.FindGameObjectWithTag("Player");
             m_targetMonsterCtrl = other.GetComponent<MonsterController>();            
         }
         else
@@ -29,9 +31,30 @@ public class PetBallController : MonoBehaviour
     {
         m_petBallInfo = InventoryManager._inst.Dict_Petball[index];
     }
+    bool IsObjectFront(Transform obj)
+    {
+        Vector3 relativePos = obj.position - m_player.transform.position;
+        if (Vector3.Dot(relativePos, m_player.transform.forward) > 0)
+            return true;
+        else
+            return false;
+    }
+
     IEnumerator CapturePet()
     {
+        if(m_targetMonsterCtrl != null)
+        {
+            Vector3 screenPos = Camera.main.WorldToScreenPoint(m_targetMonsterCtrl.transform.position);
+            transform.position = screenPos;
+            if(IsObjectFront(m_targetMonsterCtrl.transform))
+            {
+                
+            }
+            else
+            {
 
+            }
+        }
         yield return null;
     }
 }
