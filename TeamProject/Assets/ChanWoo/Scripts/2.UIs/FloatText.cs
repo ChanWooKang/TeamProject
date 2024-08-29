93570612fe6f5ef5fac904b000f2805c3033491c
ChanWooKang <mwinmwin24@naver.com>
2024-08-29 18:18:36 +0900

    ChanWoo Commit
    씬 생성

---
+++ TeamProject/Assets/ChanWoo/Scripts/2.UIs/FloatText.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public sealed class FloatText : MonoBehaviour
+{
+    public const float _lifeTime = 1.5f;
+    const string _objectName = "FloatText";
+
+    public string Text = string.Empty;
+    public float FloatSpeed = 8.0f;
+    Text _text;
+
+    public static GameObject Create(string objName, bool byPlayer, Vector3 pos, object value, float lifeTime = 0, Transform parent = null)
+    {
+        return null;
+    }
+}
