aedf901d8bd5f99481dcd1c5ab09cf84dab8acf3
enjoingmediocrity <gnrotlf@naver.com>
2024-09-02 17:10:47 +0900

    YoonHo Commit
    HudController 수정

--- TeamProject/Assets/2.Scripts/4.UIs/InfoHud/HudController.cs
+++ TeamProject/Assets/2.Scripts/4.UIs/InfoHud/HudController.cs
@@ -15,12 +15,22 @@ public class HudController : MonoBehaviour
     [SerializeField]
     Image m_bgColor;
 
-    public void InitHud(string name, int level, Color color) // 몬스터 생성과 동시에 초기화, Color - 몬스터 : 레드, 펫 : 그린
+    Vector3 m_targetPos;
+    private void Update()
+    {
+        if (isActiveAndEnabled)
+        {
+            Vector3 screenPos = Camera.main.ScreenToWorldPoint(m_targetPos);
+            transform.position = screenPos;
+        }
+    }
+    public void InitHud(string name, int level, Vector3 tartgetPos ,Color color) // 몬스터 생성과 동시에 초기화, Color - 몬스터 : 레드, 펫 : 그린
     {
         m_txtName.text = name;
         m_txtLevel.text = level.ToString();
         m_hpBar.value = 1f;
         m_bgColor.color = color;
+        m_targetPos = tartgetPos;
     }
     public void DisPlayDamage(float normalizedHp) // 데미지
     {
